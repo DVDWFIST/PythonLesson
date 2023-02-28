004dae937856eac18732c477c746a54bb666f582
Andrei Savin <mr.andr2012@gmail.com>
2023-02-28 19:34:07 +0300

    Решение задачи №16

--- Lesson_3_DZ_16.py
+++ Lesson_3_DZ_16.py
@@ -15,6 +15,7 @@ n = int(input("Введите кол-во чисел в массиве:  "))
 n_list = [randint(1, 10) for i in range(n)]
 print(n_list)
 k = int(input("Введите число для поиска:  "))
+print(k)
 n_list.count(k)
 print(f" Ваше числа встречаеться в кол-ве:{n_list.count(k)} ")
     
